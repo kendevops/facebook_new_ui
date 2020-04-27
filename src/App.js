e6872ba797e099fb5c27ded458fdd6b81c464c89
Kenneth Aladi <kennethmichael11@gmail.com>
2020-04-27 19:16:13 +0100

    Added more styles on the nav items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 function App() {
-  return <Navbar></Navbar>;
+  return (
+    <Navbar>
+      <NavItem icon="😄" />
+      <NavItem icon="😄" />
+      <NavItem icon="😄" />
+    </Navbar>
+  );
 }
 
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="navBar">
       <ul className="navBar-nav"></ul>
     </nav>
+  );
+}
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
   );
 }
 export default App;
