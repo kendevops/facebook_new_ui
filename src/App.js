afc4f75c32a52af2dd1a9ee947c9fc7f2ebe3bb0
Kenneth Aladi <kennethmichael11@gmail.com>
2020-04-27 18:55:47 +0100

    Added Nav component with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
-  return ()
+  return <Navbar></Navbar>;
 }
 
+function Navbar() {
+  return (
+    <nav className="navBar">
+      <ul className="navBar-nav"></ul>
+    </nav>
+  );
+}
 export default App;
