340905eca3b1c844e85c2579717ff3c7d05a7a6c
Kenneth Aladi <kennethmichael11@gmail.com>
2020-04-28 18:27:45 +0100

    Created the drop down feature

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon="😄" />
-      <NavItem icon="😄" />
-      <NavItem icon="😄" />
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu />
+      </NavItem>
     </Navbar>
+  );
+}
+
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-rightIcon">{props.right}</span>
+      </a>
+    );
+  }
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}
+      ></DropdownItem>
+    </div>
   );
 }
 
 function Navbar(props) {
   return (
     <nav className="navBar">
-      <ul className="navBar-nav"></ul>
+      <ul className="navBar-nav">{props.children}</ul>
     </nav>
   );
 }
 
 function NavItem(props) {
+  const [open, setOpen] = useState(false);
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+      {open && props.children}
     </li>
   );
 }
